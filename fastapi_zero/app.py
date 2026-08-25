21d87c60d0e46b389c0681b05d0eae08f4aca385
CaioWill <caio5.william5@gmail.com>
2026-08-24 22:56:02 -0300

    format

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -2,26 +2,27 @@ from http import HTTPStatus
 
 # Framework FastApi
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fastapi_zero.schemas import Mensagem
-
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
 @app.get(
-    '/', # No caminho /
-    status_code=HTTPStatus.OK, 
-    response_model=Mensagem  # schema (model) | o modelo pre definido que tem que ser enviado
-)  
+    '/',  # No caminho /
+    status_code=HTTPStatus.OK,
+    # schema (model) | o modelo pre definido que tem que ser enviado
+    response_model=Mensagem,
+)
 def read_root():
     return {'message': 'Olá mundo!'}
 
+
 @app.get(
-    '/teste', # endpoint | caminho
-    status_code=HTTPStatus.OK, 
-    response_class= HTMLResponse
+    '/teste',  # endpoint | caminho
+    status_code=HTTPStatus.OK,
+    response_class=HTMLResponse,
 )
 def read_root_teste():
     return """
--- fastapi_zero/schemas.py
+++ fastapi_zero/schemas.py
@@ -1,8 +1,8 @@
 from pydantic import BaseModel
 
 
-# Valalida a entrada e saida de dados conferindo se estão no padrão pre definido
+# Valalida in|out put de dados conferindo se estão no padrão pre definido
 # Schemas ou (models) | classe para definir a forma esperada de um dado
-# Aqui seria tipo um check do Postgres, conferindo se o dado de message é uma str
+# Aqui seria tipo um check do Postgres, confere se o dado de message é uma str
 class Mensagem(BaseModel):
     message: str
